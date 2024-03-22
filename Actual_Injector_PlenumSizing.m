122d3e371fdaf9c51e8a171eabc0ba7e54a2d523
lrpalmer27 <lrpalmer@mun.ca>
2024-03-22 10:39:48 -0230

    cleaning up codes for publishing

---
+++ Actual_Injector_PlenumSizing.m
@@ -0,0 +1,51 @@
+% DETechnologies - 2024
+% Logan Palmer
+
+%% Housekeeping
+close all force 
+clear 
+clc
+
+%% Initialize Params 
+% geometry
+OD=60e-3;
+Delta=5e-3;
+ID=OD-2*Delta;
+
+AnnulusArea=(pi()/4)*(OD^2-ID^2);
+m_dot=0.320; % kg/s total
+
+% mass flow rate of propellant
+h_massFrac=0.1116;
+O_massFrac=1-h_massFrac;
+m_dot_h=m_dot*h_massFrac;
+m_dot_O=m_dot*O_massFrac;
+
+% chemical properties
+gamma=1.4; 
+R_O=259.84; % gas constant O 
+R_H=4124.2; % gas constant H
+T_t=293; % K assume constant temp
+
+%% Init Actual Injection Areas based on DFMA / Physical Constraints
+OxyInjectorArea=12.2727/100;
+HydroInjectorArea=5.4545/100;
+
+%% Calculate Req'd Upstream Pressure
+P_upstream_O=(m_dot_O*sqrt(T_t)/OxyInjectorArea)*sqrt(R_O/gamma)*(((gamma+1)/2)^((gamma+1)/(2*(gamma-1))));
+P_upstream_H=(m_dot_h*sqrt(T_t)/HydroInjectorArea)*sqrt(R_H/gamma)*(((gamma+1)/2)^((gamma+1)/(2*(gamma-1))));
+
+% summary
+fprintf('Summary')
+fprintf("\nP_upstream_O: %f kPa",P_upstream_O)
+fprintf("\nP_upstream_H: %f kPa",P_upstream_H)
+
+
+
+
+
+
+
+
+
+
